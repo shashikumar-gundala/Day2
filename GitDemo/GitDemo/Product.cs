0ed6ee64fe321f504f04315864dcda463a838c54
shashikumar-gundala <shashikumar.gundala@lntinfotech.com>
2020-12-16 10:43:10 +0530

    Edited Product class

--- GitDemo/GitDemo/Product.cs
+++ GitDemo/GitDemo/Product.cs
@@ -1,4 +1,17 @@
-class Product{
-public void printHello(){
-Console.WriteLine("Class Added in GitHub");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    class Product
+    {
+        public void printHello()
+        {
+            Console.WriteLine("Class Added in GitHub");
+        }
+    }
 }
--- GitDemo/GitDemo/Program.cs
+++ GitDemo/GitDemo/Program.cs
@@ -10,6 +10,8 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
+            Product p = new Product();
+            p.printHello();
             Console.WriteLine("First C#");
             Console.WriteLine("Hello World");
             Console.ReadLine();
